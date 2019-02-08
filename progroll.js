94260d75c9b008c4a1ef5af0075c4b147951c757
ozhan <ozhan@ozhan-Latitude-E5450>
2019-02-08 16:04:14 +0300

    gitignore

--- progroll.js
+++ progroll.js
@@ -31,30 +31,5 @@
     
 })();
 
-    /*
-    TODO:
-        - sayfa yenilendiğinde scroll konumuna göre başlagıç width ayarlaması
-        - window rezise anında güncel kalmalı
-        - konum hatırlama cookie (gelişmiş)
-        - başladığı anda hızla görünür, durduğu anda yavaşça kaybolur (ilk scroll ile devreye girer)
-        - yenilemede sıfırdan yumuşak hareketle konumuna gelir.
-        - belirlenen süreye veya load olayını progress edebilme özelliği. (Gelişmiş)
-            Timer/Loader mode (geri/ileri sayım) özelliğinde kullanabilme 
-            başlangıç bitiş callback
-            val min max attributes
-        - tema ve animasyonlar (Gelişmiş)
-        -settings
-            updateBy //d:body /tag .class #id
-            stickTo //d:body /tag .class #id
-            sticky //d:true:fixed/false:inline
-            color //d:empty css
-            heigt //d:empty css
-            fixed/inline //d:fixed
-            position: //d:top/bottom/custom
-            smooth //d:false
-
-            
-     */
-
 
 
